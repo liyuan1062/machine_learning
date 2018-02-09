cd3941c72c8f8406af2dfa3db8318a6b38e0a1a4
Li, Lucien <lucien.li@emc.com>
2018-02-09 15:09:23 +0800

    sklearn one-hot link

--- code/Titanic/preprocess.py
+++ code/Titanic/preprocess.py
@@ -2,6 +2,7 @@
 # data analysis
 import matplotlib
 from sklearn import preprocessing
+from sklearn.preprocessing import Imputer
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -30,6 +31,7 @@ print('*'*40)
 # x = train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
 # train_df['Age'].fillna(-20, inplace=True)
 train_df['Age'].dropna(inplace=True)
+# imputer = Imputer()
 # print(train_df.count(1))
 # dy = train_df[train_df['Survived'] > 0]['Age']
 # ddy = dy[dy.isnull() == False]
@@ -55,6 +57,7 @@ for data_df in datas:
     data_df['Embarked'].fillna(data_df['Embarked'].mode()[0], inplace=True)
     data_df['Age'].fillna(data_df['Age'].dropna().median(), inplace=True)
     data_df['Fare'].fillna(data_df['Fare'].dropna().median(), inplace=True)
+    # data_df = imputer.fit_transform(data_df)
     print("null data: -->\n", data_df.isnull().sum())
 
 # create 2 new features
@@ -64,15 +67,18 @@ for data_df in datas:
     data_df['IsAlone'].loc[data_df['FamilySize'] > 1] = 0
     print("mode IsAlone: ", data_df['IsAlone'].mode())
 
+train_df = pd.get_dummies(train_df)
+test_df = pd.get_dummies(test_df)
+
 #
-label = preprocessing.LabelEncoder()
-oneHot = preprocessing.OneHotEncoder()
-for data_df in datas:
-    data_df['Sex_Code'] = label.fit_transform(data_df['Sex'])
-    data_df['Embarked_Code'] = label.fit_transform(data_df['Embarked'])
-    data_df.drop(['Sex', 'Embarked'], axis=1, inplace=True)
-    data_df.info()
-    print(data_df.head())
+# label = preprocessing.LabelEncoder()
+# oneHot = preprocessing.OneHotEncoder()
+# for data_df in datas:
+#     data_df['Sex_Code'] = label.fit_transform(data_df['Sex'])
+#     data_df['Embarked_Code'] = label.fit_transform(data_df['Embarked'])
+#     data_df.drop(['Sex', 'Embarked'], axis=1, inplace=True)
+#     data_df.info()
+#     print(data_df.head())
 # correlation heatmap
 # sns.heatmap(train_df.corr(), square=True, vmax=1.0, annot=True)
 # plt.show()
@@ -82,40 +88,40 @@ x_train = train_df.drop(['Survived', 'PassengerId'], axis=1)
 y_train = train_df['Survived']
 x_test = test_df.drop('PassengerId', axis=1)
 
-logreg = LogisticRegression()
-logreg.fit(x_train, y_train)
-y_pred_log = logreg.predict(x_test)
-acc_log = round(logreg.score(x_train, y_train)*100, 2)
-print(acc_log)
+# logreg = LogisticRegression()
+# logreg.fit(x_train, y_train)
+# y_pred_log = logreg.predict(x_test)
+# acc_log = round(logreg.score(x_train, y_train)*100, 2)
+# print(acc_log)
 
 rf = RandomForestClassifier(n_estimators=100)
 rf.fit(x_train, y_train)
 y_pred_rf = rf.predict(x_test)
 acc_rf = round(rf.score(x_train, y_train)*100, 2)
 print(acc_rf)
-# print("test_log: ", y_pred, "test_rf:", y_pred_rf)
+# print("test_log: ", y_pred_rf, "test_rf:", y_pred_rf)
 
 # svc = SVC()
 # svc.fit(x_train, y_train)
 # y_pred_svm = svc.predict(x_test)
 # acc_svm = round(svc.score(x_train, y_train)*100, 2)
 # print(acc_svm)
-# submission = pd.DataFrame({"PassengerId": test_df['PassengerId'], "Survived": y_pred_rf})
-# submission.to_csv("../../data/Titanic/test_predict_rf100.csv", index=False)
+submission = pd.DataFrame({"PassengerId": test_df['PassengerId'], "Survived": y_pred_rf})
+submission.to_csv("../../data/Titanic/test_predict_rf100_2.csv", index=False)
 
-cv = ShuffleSplit(n_splits=10, train_size=0.8, test_size=None)
-estimator = RandomForestClassifier(n_estimators=200)
-train_size, train_scores, test_scores = learning_curve(estimator, x_train, y_train, train_sizes=np.linspace(.1,1,10))
-train_mean = np.mean(train_scores, axis=1)
-train_std = np.std(train_scores, axis=1)
-
-test_mean = np.mean(test_scores, axis=1)
-test_std = np.std(test_scores, axis=1)
-plt.grid()
-plt.fill_between(train_size, train_mean-train_std, train_mean+train_std, alpha=0.1, color='r')
-plt.fill_between(train_size, test_mean-test_std, test_mean+test_std, alpha=0.1, color='g')
-plt.plot(train_size, train_mean, 'o-', color='r', label='Train score')
-plt.plot(train_size, test_mean, 'o-', color='g', label='CV score')
-plt.legend(loc='best')
-plt.show()
+# cv = ShuffleSplit(n_splits=10, train_size=0.8, test_size=None)
+# estimator = RandomForestClassifier(n_estimators=200)
+# train_size, train_scores, test_scores = learning_curve(estimator, x_train, y_train, train_sizes=np.linspace(.1,1,10))
+# train_mean = np.mean(train_scores, axis=1)
+# train_std = np.std(train_scores, axis=1)
+#
+# test_mean = np.mean(test_scores, axis=1)
+# test_std = np.std(test_scores, axis=1)
+# plt.grid()
+# plt.fill_between(train_size, train_mean-train_std, train_mean+train_std, alpha=0.1, color='r')
+# plt.fill_between(train_size, test_mean-test_std, test_mean+test_std, alpha=0.1, color='g')
+# plt.plot(train_size, train_mean, 'o-', color='r', label='Train score')
+# plt.plot(train_size, test_mean, 'o-', color='g', label='CV score')
+# plt.legend(loc='best')
+# plt.show()
 
